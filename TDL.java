05128ac04ea08786e74919bbb2acca64c8870881
chrollos web dev <106138109+chro11os@users.noreply.github.com>
2023-12-18 18:22:18 +0800

    Switch To Local Machine
    lmao

---
+++ TDL.java
@@ -0,0 +1,29 @@
+import java.util.Scanner
+public class TDL 
+{
+  public static void main(String[]args) 
+  {
+    Scanner scanner = new Scanner(System.in);
+    
+    String date;
+    String data;
+
+    System.out.println("1. Create a new to do list.");
+    System.out.println("2. Delete a list.");
+    System.out.print("Enter Choice: ");
+    String choice = scanner.nextLine();
+
+    switch (choice) {
+      case '+':
+        
+        break;
+
+      default:
+        break;
+    }
+
+
+  }
+}
+
+//ADD A TIME AND FINISH THE SWITCH CASE FOR CHOICES
