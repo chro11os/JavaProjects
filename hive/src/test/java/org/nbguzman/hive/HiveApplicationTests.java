eecc7b830d4c73f24c1177df8dcf5ccb84bb620e
chrollos <106138109+chro11os@users.noreply.github.com>
2024-10-19 04:55:34 +0800

    Added A few Stuff
    Made sql and cooordinated everything

---
+++ hive/src/test/java/org/nbguzman/hive/HiveApplicationTests.java
@@ -0,0 +1,14 @@
+package org.nbguzman.hive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
---
+++ hiveUI/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {ProductPageComponent} from './product-page/product-page.component';
+import {Product} from './product/model';
+import {ProductService} from './product/service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ProductPageComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  products: Product[] = [];
+  constructor(private productService: ProductService) {}
+
+  title = 'hiveUI';
+
+  ngOnInit() {
+    this.productService.getAllProducts().subscribe(data => {
+      this.products = data;
+      console.log(this.products);  // Check the products fetched from the backend
+    });
+  }
+}
