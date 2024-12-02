2d705fb65414bdde04e691212562b72268802dc8
chro11os <neilbragsg@gmail.com>
2024-12-03 00:15:05 +0800

    added practice to the main repository, for my practice codes also added a upcoming project using sprongboot, angular and mysql API

--- PracticeCodes/src/main/java/main.java
+++ PracticeCodes/src/main/java/main.java
@@ -6,21 +6,26 @@ public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         outputs puts = new outputs();
-
+        char choice;
         float userinputs;
         float userinputs2;
 
+    do {
         System.out.print("Enter 1st Number: ");
         userinputs = scanner.nextFloat();
 
         System.out.print("Enter 2nd Number: ");
         userinputs2 = scanner.nextFloat();
-
+        
         System.out.print("Enter an Operation: + Addition, - Subtraction, * Multiplication, / Division, % Modulus: ");
         char op = scanner.next().charAt(0);
-        scanner.close();
 
         puts.calculate(userinputs, userinputs2, op);
-        System.out.println ("Result: " + puts.getResults(userinputs, userinputs2));
+        System.out.println("Result: " + puts.getResults(userinputs, userinputs2));
+
+        System.out.print("Do you Want to Continue? (Y/N): ");
+        choice = scanner.next().charAt(0);
+    } while (choice == 'Y' || choice == 'y');
+
     }
 }
